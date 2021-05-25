d3a607ac343ae2b9d1ede8eb1ab7995a9786eada
Sadaf Perveen <70029385+sadaf-jamal-au16@users.noreply.github.com>
2021-05-25 21:11:04 +0530

    done

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("Request Received");
 });
+app.get("/login", (req, res) => {
+  res.json("Both of Us have created our own branches");
+});
 
 app.listen(3000, () => {
   console.log("Server Connected");
