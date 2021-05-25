953c370e9a2d863a022e5e0d2249a1a157a22254
Sadaf Perveen <70029385+sadaf-jamal-au16@users.noreply.github.com>
2021-05-25 21:51:53 +0530

    done

--- server.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-app.get("/", (req, res) => {
-  res.send("Request Received");
-});
-
-app.listen(3000, () => {
-  console.log("Server Connected");
-});
