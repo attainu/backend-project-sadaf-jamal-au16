668b77916104063a76a8f13fecd0074bee860afa
Sunil <suniljoshi.sj1@gmail.com>
2021-05-28 00:03:47 +0530

    folder structure deleted to add new

--- config/keys.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    mongoURI: "mongodb+srv://demo:12345@cluster0.6ffyh.mongodb.net/Hungry?retryWrites=true&w=majority"
-    // secretOrKey: "secret",
-    // userRoles: ["user", "manager"]
-  };
