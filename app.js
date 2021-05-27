ab9597ee2b435408cc50e807ecc0a0bc99e895a9
Sunil <suniljoshi.sj1@gmail.com>
2021-05-27 23:54:20 +0530

    feat: login page, signup page, partner signup page, partner dashboard

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
 const express = require('express');
-// const morgan = require('morgan');
+
 const expHbs = require('express-handlebars');
+
+const mongoose = require('mongoose');
+const db = require("./config/keys").mongoURI;
+
+const sellerRoutes = require('./routes/sellerDashboard');
+
 const app = express();
 
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }));
 app.engine('hbs', expHbs({ extname: 'hbs'}));
 app.set('view engine', 'hbs');
 
+mongoose.connect(db, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+})
+.then(() => console.log("MongoDB successfully connected"))
+.catch(err => console.log(err));
+
 app.get('/', (req, res) => {
-    res.render('home')
+    res.render('home');
 })
 
 app.get('/login', (req, res) => {
@@ -19,4 +35,7 @@ app.get('/signUp', (req, res) => {
     res.render('signup');
 })
 
-app.listen(3000, () => console.log('server started'));
+app.use('/seller', sellerRoutes)
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server up and running on port ${port} !`));
---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI: "mongodb+srv://demo:12345@cluster0.6ffyh.mongodb.net/Hungry?retryWrites=true&w=majority"
+    // secretOrKey: "secret",
+    // userRoles: ["user", "manager"]
+  };
